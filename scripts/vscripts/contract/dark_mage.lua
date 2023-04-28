0a664cae0bee3b10173254927773859ba007fd7b
LSPGLHB <231776007@qq.com>
2023-04-28 18:23:12 +0800

    v2023.4.28

--- scripts/vscripts/contract/dark_mage.lua
+++ scripts/vscripts/contract/dark_mage.lua
@@ -1,12 +1,13 @@
 require('player_power')
-function modifier_contract_battle_magician_on_created(keys)
+function modifier_contract_dark_mage_on_created(keys)
     print("onCreated_battle_magician")
     refreshContractBuff(keys,true)
 end
 
-function modifier_contract_battle_magician_on_destroy(keys)
+function modifier_contract_dark_mage_on_destroy(keys)
     print("onDestroy_battle_magician")
     refreshContractBuff(keys,false)
+
 end
 
 function refreshContractBuff(keys,flag)
@@ -15,28 +16,31 @@ function refreshContractBuff(keys,flag)
     local playerID = caster:GetPlayerID()
     
  
-    local contract_ability_speed_precent_final = ability:GetSpecialValueFor( "contract_ability_speed_precent_final")
+    local contract_mana = ability:GetSpecialValueFor( "contract_mana")
     local contract_range_precent_final = ability:GetSpecialValueFor( "contract_range_precent_final")
+    local contract_ability_speed_precent_final = ability:GetSpecialValueFor( "contract_ability_speed_precent_final")
     local contract_damage_precent_final = ability:GetSpecialValueFor( "contract_damage_precent_final")
     local contract_control_precent_final = ability:GetSpecialValueFor( "contract_control_precent_final")
+    local contract_vision = ability:GetSpecialValueFor( "contract_vision")
+    local contract_health_precent_final = ability:GetSpecialValueFor( "contract_health_precent_final")
+    local contract_speed = ability:GetSpecialValueFor( "contract_speed")
 
-    local contract_vision_flag = ability:GetSpecialValueFor( "contract_vision_flag")
-    local contract_speed_flag = ability:GetSpecialValueFor( "contract_speed_flag")
-    local contract_health_flag = ability:GetSpecialValueFor( "contract_health_flag")
 
-    if (not flag) then
-        contract_vision_flag = 1
-        contract_speed_flag = 1
-        contract_health_flag = 1
-    end
 
-    setPlayerPower(playerID, "player_ability_speed_C_precent_final", flag, contract_ability_speed_precent_final)
-    setPlayerPower(playerID, "player_ability_speed_B_precent_final", flag, contract_ability_speed_precent_final)
-    setPlayerPower(playerID, "player_ability_speed_A_precent_final", flag, contract_ability_speed_precent_final)
+
+
+    setPlayerPower(playerID, "player_mana", flag, contract_mana)
+    setPlayerPower(playerID, "player_vision", flag, contract_vision)
+    setPlayerPower(playerID, "player_health_precent_final", flag, contract_health_precent_final)
+    setPlayerPower(playerID, "player_speed", flag, contract_speed) 
 
     setPlayerPower(playerID, "player_range_C_precent_final", flag, contract_range_precent_final)
     setPlayerPower(playerID, "player_range_B_precent_final", flag, contract_range_precent_final)
     setPlayerPower(playerID, "player_range_A_precent_final", flag, contract_range_precent_final)
+
+    setPlayerPower(playerID, "player_ability_speed_C_precent_final", flag, contract_ability_speed_precent_final)
+    setPlayerPower(playerID, "player_ability_speed_B_precent_final", flag, contract_ability_speed_precent_final)
+    setPlayerPower(playerID, "player_ability_speed_A_precent_final", flag, contract_ability_speed_precent_final)
 
     setPlayerPower(playerID, "player_damage_C_precent_final", flag, contract_damage_precent_final)
     setPlayerPower(playerID, "player_damage_B_precent_final", flag, contract_damage_precent_final)
@@ -47,14 +51,11 @@ function refreshContractBuff(keys,flag)
     setPlayerPower(playerID, "player_control_A_precent_final", flag, contract_control_precent_final)
 
 
-    setPlayerPowerFlag(playerID, "player_vision_flag", contract_vision_flag)
-    setPlayerPowerFlag(playerID, "player_speed_flag", contract_speed_flag)
-    setPlayerPowerFlag(playerID, "player_health_flag", contract_health_flag)
 
-
+    setPlayerBuffByNameAndBValue(caster,"mana",GameRules.playerBaseMana)
     setPlayerBuffByNameAndBValue(caster,"vision",GameRules.playerBaseVision)
-    setPlayerBuffByNameAndBValue(caster,"speed",GameRules.playerBaseSpeed)
     setPlayerBuffByNameAndBValue(caster,"health",GameRules.playerBaseHealth)
+    setPlayerBuffByNameAndBValue(caster,"speed",GameRules.playerBaseSpeed)
 
 end
 
---
+++ scripts/vscripts/contract/scout.lua
@@ -0,0 +1,44 @@
+require('player_power')
+function modifier_contract_scout_on_created(keys)
+    print("onCreated_battle_magician")
+    refreshContractBuff(keys,true)
+end
+
+function modifier_contract_scout_on_destroy(keys)
+    print("onDestroy_battle_magician")
+    refreshContractBuff(keys,false)
+
+end
+
+function refreshContractBuff(keys,flag)
+    local caster = keys.caster
+    local ability = keys.ability
+    local playerID = caster:GetPlayerID()
+    
+ 
+    local contraccontract_health_precent_finalt_mana = ability:GetSpecialValueFor( "contract_health_precent_final")
+    local contract_vision = ability:GetSpecialValueFor( "contract_vision")
+    local contract_range_flag = ability:GetSpecialValueFor( "contract_range_flag")
+    local contract_ability_speed_flag = ability:GetSpecialValueFor( "contract_ability_speed_flag")
+    local contract_damage_flag = ability:GetSpecialValueFor( "contract_damage_flag")
+    local contract_control_flag = ability:GetSpecialValueFor( "contract_control_flag")
+    local contract_energy_flag = ability:GetSpecialValueFor( "contract_energy_flag")
+
+
+
+    setPlayerPower(playerID, "player_mana", flag, contract_health_precent_final)
+    setPlayerPower(playerID, "player_vision", flag, contract_vision)
+
+    setPlayerPowerFlag(playerID, "player_range_flag", contract_range_flag)
+    setPlayerPowerFlag(playerID, "player_ability_speed_flag", contract_ability_speed_flag)
+    setPlayerPowerFlag(playerID, "player_damage_flag", contract_damage_flag)
+    setPlayerPowerFlag(playerID, "player_control_flag", contract_control_flag)
+    setPlayerPowerFlag(playerID, "player_energy_flag", contract_energy_flag)
+
+    setPlayerBuffByNameAndBValue(caster,"mana",GameRules.playerBaseMana)
+    setPlayerBuffByNameAndBValue(caster,"vision",GameRules.playerBaseVision)
+
+
+end
+
+
